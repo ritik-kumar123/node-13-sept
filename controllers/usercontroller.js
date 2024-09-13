69f51989e09ceab947e3eeb15e275e410eb91d90
ritik-kumar123 <anujkumar73514@gmail.com>
2024-09-13 11:01:28 +0530

    uplodaing the first version

---
+++ controllers/usercontroller.js
@@ -0,0 +1,15 @@
+const users=require('../users.json');
+function getAllUsers(req,res)
+{
+    try
+    {
+    res.json(users)
+    }
+    catch(err)
+    {
+
+    }
+}
+module.exports={
+    getAllUsers
+}
