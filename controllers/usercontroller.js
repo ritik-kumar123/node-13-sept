10fb8d8082e3c2b1be6ccf48f769500caebedcae
ritik-kumar123 <anujkumar73514@gmail.com>
2024-09-13 12:49:54 +0530

    updation

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -10,6 +10,22 @@ function getAllUsers(req,res)
 
     }
 }
+function getuser(req,res)
+{
+    
+    try
+    {
+        let id =parseInt(req.params.id)
+        let user=users.find((user)=>user.id===id);
+        res.json(user)
+    }
+    catch(err)
+    {
+
+    }
+
+}
 module.exports={
-    getAllUsers
+    getAllUsers,
+    getuser
 }
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,10 @@ router.get('/users',(req,res)=>
 usercontrollers.getAllUsers(req,res);
 
 })
+router.get('/user:id',(req,res)=>
+{
+usercontrollers.getUser(req,res);
+
+})
 
 module.exports = router
